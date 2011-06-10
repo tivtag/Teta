3eb80729736e7250b785b99e198badae61e4eb92
tivtag <tick@federrot.at>
2011-06-11 00:27:04 +0200

    More refactoring.

--- lib/mechanics/goto.rb
+++ lib/mechanics/goto.rb
@@ -3,18 +3,8 @@ module Mechanics
   module Goto
 
     def goto(location_name)
-      available_locations = location.connected_locations
-
-      if location_name then
-        loc = available_locations.find {|child| child.name == location_name.intern}
-
-        # Allow the user to enter 'fi' to go to 'fire':
-        if not loc then
-          loc = available_locations.find {|child| child.name.to_s.start_with_any? location_name } 
-        end
-      else
-        loc = nil
-      end
+      available = location.connected_locations
+      loc = find_location(location_name, available)
 
       if loc then
         change_location loc
@@ -23,7 +13,7 @@ module Mechanics
         print_location
       else
         puts "I can't go there." unless location_name == nil
-        available_locations.each {|l| puts "    #{l.name}"}        
+        available.each {|l| puts "    #{l.name}"}        
       end
     end
 
@@ -39,6 +29,22 @@ module Mechanics
       else
         puts "I can't do that."
       end
+    end
+
+  private
+    def find_location(name, available)
+      if name then
+        loc = available.find {|child| child.name == name.intern}
+
+        # Allow the user to enter 'fi' to go to 'fire':
+        if not loc then
+          loc = available.find {|child| child.name.to_s.start_with_any? name } 
+        end
+      else
+        loc = nil
+      end
+
+      loc
     end
 
   end
