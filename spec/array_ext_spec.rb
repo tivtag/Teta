79c3fa151c69c72839b936bbd18281a53c1bdccb
tivtag <tick@federrot.at>
2011-11-21 00:35:34 +0100

    Fixed broken specs. Those didn't do anything.

--- spec/array_ext_spec.rb
+++ spec/array_ext_spec.rb
@@ -4,15 +4,15 @@ describe Array, "Extensions" do
 
   describe '#append_unique!' do
     it 'returns a singleton when appending to an empty array' do
-      [].append_unique! 1 == [1]   
+      ([].append_unique! 1).should == [1]   
     end
 
     it 'returns the array unchanged when appending an existing item' do
-      [1,2,3].append_unique! 2 == [1,2,3]
+      ([1,2,3].append_unique! 2).should == [1,2,3]
     end
 
     it 'returns the array with the item appended at the end when appending a not yet existing item' do
-      [1,2,3].append_unique! 0 == [1,2,3,0]
+      ([1,2,3].append_unique! 0).should == [1,2,3,0]
     end
   end
 
